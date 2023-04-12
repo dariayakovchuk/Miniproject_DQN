e9d32a07c6a76c3820101c3b973ed7abfd350c85
Daria <daria.yakovchuk@epfl.ch>
2023-04-12 16:38:12 +0300

    agent DQL

--- Miniproject_DQN/DQ_learning/agent.py
+++ Miniproject_DQN/DQ_learning/agent.py
@@ -84,3 +84,92 @@ class Agent(ABC):
         Returns:
             Tuple[int, float]: the selected action (as an int) and associated Q/V-value as a float
         """
+
+class DQNAgent(Agent):
+    def __init__(self,  env:Env,
+                 model:torch.nn,
+                 criterion=nn.SmoothL1Loss(),
+                 lr:float=5e-3,
+                 epsilon:float=0.7,
+                 gamma:float=0.9,
+                 buffer_size:int=20000,
+                 batch_size:int=2048)->None:
+
+        self.env = env
+        state, info = self.env.reset()
+
+        model_params = {
+            'n_observations': len(state),
+            'n_actions': env.action_space.n,
+        }
+        
+        self.model = model(**model_params)
+        self.targetnet = model(**model_params)
+
+        self.criterion = criterion
+        self.optimizer = optim.AdamW(self.model.parameters(), lr=lr, amsgrad=True)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.memory = ReplayMemory(buffer_size)
+        self.batch_size = batch_size
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.lr = lr
+        
+    def optimize_model(self):
+        if len(self.memory) < self.batch_size:
+                return np.double(0)
+            
+        transitions = self.memory.sample(self.batch_size)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.model(state_batch).gather(1, action_batch.unsqueeze(1))
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.targetnet(non_final_next_states).max(1)[0]
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        loss = self.criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), 100)
+        self.optimizer.step()
+        
+        return loss
+    
+    def reset():
+        pass
+    
+    def act(self, state):
+        epsilon = self.epsilon
+        sample = random.random()
+        
+        if sample > epsilon:
+            with torch.no_grad():
+                return self.model(state).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[self.env.action_space.sample()]], device=self.device, dtype=torch.long)
