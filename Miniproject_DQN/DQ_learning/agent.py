a35347cf4e4524b7888eae25eb50c3ff08b983ef
Daria <daria.yakovchuk@epfl.ch>
2023-04-20 10:17:39 +0300

    with saving of the model

--- Miniproject_DQN/DQ_learning/agent.py
+++ Miniproject_DQN/DQ_learning/agent.py
@@ -106,6 +106,7 @@ class DQNAgent(Agent):
         
         self.policy_net = model(**model_params)
         self.target_net = model(**model_params)
+        # self.target_net.load_state_dict(self.policy_net.state_dict())
 
         self.criterion = criterion
         self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=lr, amsgrad=True)
@@ -118,9 +119,12 @@ class DQNAgent(Agent):
         self.lr = lr
         self.T_max = n_episodes
 
-    def load_model(self, savepath:str): pass
+    def load_model(self, savepath:str): 
+        torch.save(self.policy_net.state_dict(), savepath)
+        self.policy_net.load_state_dict(torch.load(savepath))
 
-    def save_model(self, savepath:str): pass
+    def save_model(self, savepath:str):
+        torch.save(self.policy_net.state_dict(), savepath)
 
     def optimize_model(self, update = True):
         if len(self.memory) < self.batch_size:
@@ -168,7 +172,107 @@ class DQNAgent(Agent):
         self.optimizer.step()
         return loss
 
-    def reset():
+    def reset(self):
+        pass
+
+    def act(self, state, time, decreasing = False):
+        epsilon = self.epsilon
+        if decreasing:
+            epsilon = max(self.epsilon * (self.T_max - time) / self.T_max, self.epsilon_min)
+        sample = random.random()
+        if sample > epsilon:
+            with torch.no_grad():
+                return self.policy_net(state).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[self.env.action_space.sample()]], device=self.device, dtype=torch.long)
+        
+        
+        
+class DQNAgent_Factorize(Agent):
+    def __init__(self,  env:Env,
+                 model:torch.nn,
+                 criterion=nn.SmoothL1Loss(),
+                 lr:float=5e-3,
+                 epsilon:float=0.7,
+                 gamma:float=0.9,
+                 buffer_size:int=20000,
+                 batch_size:int=2048,
+                 n_episodes:int=500, 
+                 epsilon_min:float=0.3)->None:
+
+        self.env = env
+        
+        model_params = {
+            'n_observations': 189,
+            'n_actions': 4*2,
+        }
+        
+        self.policy_net = model(**model_params)
+        self.target_net = model(**model_params)
+        # self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.criterion = criterion
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=lr, amsgrad=True)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.memory = ReplayMemory(buffer_size)
+        self.batch_size = batch_size
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.gamma = gamma
+        self.lr = lr
+        self.T_max = n_episodes
+
+    def load_model(self, savepath:str): 
+        torch.save(self.policy_net.state_dict(), savepath)
+        self.policy_net.load_state_dict(torch.load(savepath))
+
+    def save_model(self, savepath:str):
+        torch.save(self.policy_net.state_dict(), savepath)
+
+    def optimize_model(self, update = True):
+        if len(self.memory) < self.batch_size:
+                return np.double(0)
+        transitions = self.memory.sample(self.batch_size)
+        batch = Transition(*zip(*transitions))
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = torch.sum(self.policy_net(state_batch).gather(1, action_batch), axis=2)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size, device=self.device)
+        if update: 
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
+        else: 
+            with torch.no_grad():
+                next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
+
+        # Compute Huber loss
+        loss = self.criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
+        return loss
+
+    def reset(self):
         pass
 
     def act(self, state, time, decreasing = False):
--- Miniproject_DQN/DQ_learning/model.py
+++ Miniproject_DQN/DQ_learning/model.py
@@ -7,16 +7,30 @@ import torch.nn.functional as F
 class DQN(nn.Module):
 
     def __init__(self, n_observations, n_actions):
-        super(DQN, self).__init__()
+        super().__init__()
         self.layer1 = nn.Linear(n_observations, 64)
         self.layer2 = nn.Linear(64, 32)
         self.layer3 = nn.Linear(32, 16)
         self.layer4 = nn.Linear(16, n_actions)
 
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         x = F.relu(self.layer3(x))
         return self.layer4(x)
+
+
+class DQN_Factorized(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super().__init__()
+        self.layer1 = nn.Linear(n_observations, 64)
+        self.layer2 = nn.Linear(64, 32)
+        self.layer3 = nn.Linear(32, 16)
+        self.layer4 = nn.Linear(16, 4*2)
+        
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+        y = self.layer4(x).view(-1, 4, 2)
+        return y #, torch.max(y, axis = 1)
